14750e9ef65461551d1b1f762f95f8a2ce68a219
Mitch Masterson <mitchell.masterson@gmail.com>
2016-08-16 23:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var evens = [2, 4, 6, 8, 10]
-
-evens.forEach(even => {
-  console.log(`${even} is not odd!`)
-})
-
 function doToElementsInArray(array, callback) {
   array.forEach(callback)
 }
 
 function changeCompletely(element, index, array) {
-  array[index] = (Math.random() * 100).toString + '!!!'
+  array[index] = element + '!!!'
 }
