ae429107bc2fd6b44e84241ca4b02ea72dfb2e3e
David Neff <davemneff@gmail.com>
2016-08-20 16:54:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function doToElementsInArray(array, callback) {
   array.forEach(callback)
 }
+
 function changeCompletely(element, index, array) {
   array[index] = (Math.random() * 100).toString() + '!!!'
 }
@@ -9,5 +10,4 @@ var animals = ["dog", "fish", "cat"]
 
 doToElementsInArray(animals, changeCompletely)
 
-// log out animals -- pretty cool, right?
 console.log(animals)
