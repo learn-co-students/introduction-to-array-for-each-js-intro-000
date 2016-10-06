a78c6a18a160b8d4ab5ec630b5a13c75d98da0ec
p sc <patty.stacruz@gmail.com>
2016-10-06 16:57:38 +0000

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,3 @@ function changeCompletely(element, index, array) {
   array[index] = (Math.random() * 100).toString() + '!!!'
 }
  
-var animals = ["dog", "fish", "cat"]
- 
-doToElementsInArray(animals, changeCompletely)
- 
-console.log(animals)
