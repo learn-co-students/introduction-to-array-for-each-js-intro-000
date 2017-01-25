73bb2fc01723059b4ddf94b1fb03f3dd49ddfe31
Yujun Sung <yujunsung@gmail.com>
2017-01-25 02:43:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeCompletely(element, index, array){
+
+  array[index] = (Math.random() * 100).toString() + '!!!'
+
+
+}
+
+
+function doToElementsInArray(array, callback){
+
+  console.log(array);
+  //console.warn(array);
+
+
+
+}
