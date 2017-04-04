9322955f0c25955eb160dd00ce0419f5f00ebbb5
Chris D <erailtrainsoftware@gmail.com>
2017-04-04 04:45:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function changeCompletely(element, index, array){
   var result = [];
   array.forEach(function(){
-    result.push(index[element]);
+    result.push(element);
   });
   return result;
 }
