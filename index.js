43a026c5ba32cc8d4afe1da0fb283fe5c7b49ab2
Daniel Alviso <dan.alviso2@yahoo.com>
2017-06-08 22:55:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var evens = [0, 2, 4, 6, 8, 10];
+var animals = ["dog", "fish", "cat"];
+
+function doToEvens(callback) {
+  evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+doToElementsInArray(animals, changeCompletely);
+console.log(animals);
