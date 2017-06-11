2a5c81c32178fa27425e9c0a2da845c0a6ea231a
Máté Rajnai <ramate.rajnai@gmail.com>
2017-06-11 12:53:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+function changeCompletely(element, index, array) {array[index] = (Math.random() * 100).toStrings() + '!!!'}
+function doToElementsInArray(array, callback) {array.forEach(callback)}
+end()
