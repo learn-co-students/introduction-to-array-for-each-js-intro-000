b473449d5366abce0e718e0e04f2f08c4dbded2a
Neena van de Water <nkuriakose@hotmail.com>
2017-06-22 22:47:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function changeCompletely(element, index, array){
+  var what = "1";
+
+}
+
+function doToElementsInArray(array, callback){
+return array.forEach(callback)
+}
+// array.forEach(function(item)){
+//   return changeCompletely()
+// }
