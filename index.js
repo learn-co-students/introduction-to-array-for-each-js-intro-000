ccd18b72e3284821e2408c2f83f55076c9c6bb35
Kelly Stratton <kclarencestratton@gmail.com>
2017-06-26 16:34:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+evens = [0,2,4,6,8,10]
+
+function square(n) {
+    console.log(n * n)
+}
+function doToEvens(callback) {
+    evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback) {
+    array.forEach(callback)
+}
+
+function changeCompletely(element, index, array){
+  array[index] = (Math.random()*100).toString() + `!!!`
+}
