50d963b2ce6e7f0a157a6183b3975acf4620229e
L North <lnordell@gmail.com>
2017-08-23 01:57:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-var evens = [0, 2, 4, 6, 8, 10]
 
-evens.forEach(even => {
-  console.log(`${even} is not odd!`)
-})
+// Add your doToElementsInArray() function here:
 
-evens.forEach((even, index, array) => {
-  debugger
-  console.log(`${even} is not odd!`)
-})
-
-function square(n) {
-  console.log(n*n)
+function doToElementsInArray(array,callback) {
+  array.forEach(callback);
 }
-
-//no need to redeclare if you've declared above
-
-function doToEvens(callback){
-  evens.forEach(callback)
-}
-
-function doToElementsInArray(array, callback){
-  array.forEach(callback)
-}
-
+// Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
-  array[index] = (Math.random()*100).toString() + '!!!'
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
 
-var animals = ['dog', 'fish', 'cat']
+var animals = ["dog", "cat", "squirrel"];
 
-doToElementsInArray(animals, changeCompletely)
+doToElementsInArray(animals, changeCompletely);
 
-console.log(animals)
+console.log(animals);
