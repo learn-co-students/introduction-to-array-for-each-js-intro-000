1eeee5bc10c078ac93e04fefb7fa76ff86f57231
alec <alec@petros.audio>
2017-12-11 17:36:30 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
 }
+// Getting a variety of strange errors in the test, seems unrelated to the code I added
+// Local package.json exists, but node_modules missing, did you mean to install?
+// Test isn't marking as completed in my console, but learn.co considers it passed.
+// Chalking this up to an error, since it submitted fine, and moving on?
