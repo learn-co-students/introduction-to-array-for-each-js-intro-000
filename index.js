1e49df567724308b239bff0fbf1453b2f4d3a0b6
First Last <flatironcamp.tc99@mamber.net>
2018-01-15 05:53:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, func) {
+  //console.log("in doToElementsInArray()");
+  array.forEach(func);
+}
 
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  //console.log("in changeCompletely()");
+  array[index] = Math.floor(Math.random() * 100 + 2).toString()
+    + ` ${array[index]}s!!!`;
+}
+
+var animals = ["bear", "pig", "platypus", "frog"];
+
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);
