e37f50c6060be379d83f04d099d7dbc5d14d3ef8
David Chapman <chap6680misc@gmail.com>
2018-01-20 07:36:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// Add your doToElementsInArray() function here:
+// Define a function, iterativeLog(), that accepts an array. Call .forEach() on this array, and inside the callback, log each element with the format ${index}: ${element}.
+// Define a function, iterate, that accepts a callback. Within the iterate() function, you should initialize an array. It can contain anything you want, but make sure that it is not empty! Call .forEach() on this array, passing the callback to .forEach(). Then return the array that you initialized.
+// Define a function, doToArray that accepts an array and a callback. Call .forEach() on the array, passing the callback as the forEach callback.
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
+function iterativeLog(getA) {
+  getA.forEach(function(element,index) {
+    console.log(`${index}: ${element}`);
+  });
+}
 
-// Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
