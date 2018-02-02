e809ad03ac28b75859c6ada1e7ab52a170e937fa
bkhartmire <bkhartmire@gmail.com>
2018-02-02 22:39:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  return array.forEach(callback)
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(elements, index, array) {
+  array[index] = "hello"
+}
