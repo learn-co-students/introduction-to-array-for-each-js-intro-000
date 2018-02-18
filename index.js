582e2b2b4f85dc11e009f287b1f2b48bcee13c7a
Alex Segers <a.segers.dev@gmail.com>
2018-02-18 01:41:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
 
-// Add your changeCompletely() function here:
+// riverdaleMainCharacters;
+// => ["7 Jugheads!!!", "42 Bettys!!!", "11 Veronicas!!!", "35 Archies!!!", "50 Cheryls!!!"]
