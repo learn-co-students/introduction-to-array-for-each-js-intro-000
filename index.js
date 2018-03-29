edf77c47e22583f018bbbacb0ad8681d7fd0f9b3
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-29 19:06:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback){
+    array.forEach(callback);
 
+}
 
-// Add your changeCompletely() function here:
+function changeCompletely(element,index, array){
+  array[index] =` ${array[index]}!`;
+
+}// Add your changeCompletely() function here:
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
