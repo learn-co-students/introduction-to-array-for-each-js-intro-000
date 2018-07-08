42d4b7347dd306b6cae282e1641e8b60f9ca057f
eggsvonsatan <elbadry.d@gmail.com>
2018-07-08 01:59:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
+function changeCompletely(element, index, array){
+ array[index] = array[index].toUpperCase();
+};
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
+};
 
 
 // Add your changeCompletely() function here:
