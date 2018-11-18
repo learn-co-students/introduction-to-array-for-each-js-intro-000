55b0f590b40ff7fb34dfff62937e0a4fdac759ed
Fei Qi <feiqi9047@gmail.com>
2018-11-18 22:34:06 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,35 @@
 
 
 // Add your changeCompletely() function here:
+
+
+var evens = [0, 2, 4, 6, 8, 10];
+
+evens.forEach(
+  even => {
+  console.log(`${even} is not odd!`);
+});
+
+function square(n){
+  console.log(n*n)
+}
+
+evens.forEach(square)
+
+function doToEvens(callback){
+  evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
+function changeCompletely(element, index, array){
+  array[index] = Math.floor(Math.random()*100+2).toString() + `${array[index]}s!!!`
+}
+
+doToElementsInArray(animals, changeCompletely)
+
+console.log(animals)
