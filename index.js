97f7dfd21e071208bc9bc75ca4237e2a197959d3
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-15 18:09:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
 
-
+function doToEvens(callback){
+  even.forEach(callback);
+}
+/* Now we can pass any function to doToEvens and that function will be invoked on every element of the evens array!
+*/
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 // Add your changeCompletely() function here:
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
