62e549943b59105cad512c79721ba5a76b24dbd2
Jake Rose <jake1020@mac.com>
2019-11-16 21:02:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
 
-
+function doToElementsInArray(callback) {
+  evens.forEach(callback);
+}
 // Add your changeCompletely() function here:
+function changeCompletely(callback) {
+  evens.forEach(callback);
+}
