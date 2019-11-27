ace8c0ac2309258592543910779a835568465b91
al <albliu@email.com>
2019-11-27 01:15:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // Add your doToElementsInArray() function here:
+/*
+function doToElementsInArray(arr){
+
+  arr.forEach(changeCompletely)
+
+}//end doToElementsInArray
+
+function changeCompletely(){
 
 
-// Add your changeCompletely() function here:
+}*/
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
