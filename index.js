b20ec2eeeb211b974e7abffc423bec24d524ccf3
James Stamps Jr <jstampshere@gmail.com>
2020-02-16 02:05:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
+}
 
 
-// Add your changeCompletely() function here:
+
+var animals = ["dog", "cat", "squireel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);
