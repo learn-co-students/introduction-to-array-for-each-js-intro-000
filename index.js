69d2254c36764464131ac2e9cbcd4fef61d10fae
MJU <marie.ju2012@outlook.com>
2020-03-22 22:30:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Add your doToElementsInArray() function here:
+// define a function that accepts two arguments:
+// an array and a callback function to pass to .forEach()
+doToElementsInArray(array, changeCompletely) {
+  console.log(array)
+}
 
-
-// Add your changeCompletely() function here:
+changeCompletely(element, index, array) {
+  array[index] = `Mmmm, ${element}!!!`
+}
