7d85b7abdd614670fa64dfae8f9b07d4bbda6a50
MJU <marie.ju2012@outlook.com>
2020-03-23 01:34:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// define a function that accepts two arguments:
-// an array and a callback function to pass to .forEach()
-doToElementsInArray(array, changeCompletely) {
-  console.log(array)
+// define a function doToElementsInArray() that accepts two arguments:
+//  an array and a callback function to pass to .forEach()
+// doToElementsInArray() invokes the passed-in callback function on every element
+//  of the passed-in array using Array.prototype.forEach():
+
+function doToElementsInArray(array, changeCompletely) {
+  array.forEach(changeCompletely)
 }
 
-changeCompletely(element, index, array) {
+// changeCompletely() is defined
+// changeCompletely() alters every element in an array when used in conjunction with
+// Array.prototype.forEach()
+function changeCompletely(element, index, array) {
   array[index] = `Mmmm, ${element}!!!`
 }
