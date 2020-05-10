3c8279fcd0689e700aa11baf7c223d2471e4b5c6
Allen <developer2019@icloud.com>
2020-05-10 20:37:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
 
+const doToElementsInArray = (array, callback) => {
+  array.forEach(callback);
+  return `Mmmm, ${fruit}!!!`
+}
+
 
 // Add your changeCompletely() function here:
