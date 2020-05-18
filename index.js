e0f625cb8908002d78a779d12d0ec32c5fc3bfe8
Ladeeluc <laura.hatch.lh@gmail.com>
2020-05-18 11:38:50 -0500

    adds comments to code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// Add your doToElementsInArray() function here:
-function doToElementsInArray(array, callback) {
+// Add your doToElementsInArray() function here:// sets function  in globally scope to use callback for other functions
+function doToElementsInArray(array, callback) { //to use callback for other functions
   array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+function changeCompletely(element, index, array) {// function uses array index parameter 
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;// to create greatest integer at random;
+  //  multiplies by 100 adds 2 then changes to string to print string intorpalated 
 }
 
-var animals = ["dog", "cat", "squirrel"];
+var animals = ["dog", "cat", "squirrel"];// sets variable of array 
 
-doToElementsInArray(animals, changeCompletely);
-
+doToElementsInArray(animals, changeCompletely);// fuction uses previous function on array of animals
+//in callback, passes another function, console log result
 // log out animals -- pretty cool, right?
-console.log(animals);
+console.log(animals); // Array(3) ["8 dogs!!!", "44 cats!!!", "95 squirrels!!!"]
