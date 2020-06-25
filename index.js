d9eaafb108c91ca9a2b3fd50cf8e068b6bcef9ce
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-25 00:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(arr, callback) {
+  arr.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = 5 + index;
+}
