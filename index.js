2e70367533cc6b21bda178c9893cdaf0bd9863f4
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-11 05:46:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
-function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+function changeCompletely(element,index,array) {
+  array[index] = Math.floor(Math.random()*100).toString() + ` ${array[index]}s!!!`;
 }
