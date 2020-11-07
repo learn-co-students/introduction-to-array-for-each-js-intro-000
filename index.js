2b1c21d5a5d07261f546336450ac330966701fb2
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 12:08:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(arr, myFunc) {
+  arr.forEach(myFunc);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(item, index, array) {
+  array[index] = `${item}!!!`;
+}
