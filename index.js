e9e3b2ab779abcd1c1e37308c544c2b3cad5e516
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 17:51:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(arr, call) {
+  arr.forEach(call);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(elm, idx, arr) {
+  arr[idx] = Math.floor(Math.random() * 100 + 2).toString() + ` ${arr[idx]}s!!!`;
+}
