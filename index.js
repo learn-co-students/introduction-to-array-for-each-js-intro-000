7fbe77264d1fda4dd4fe429be4f91594f07082ac
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 21:51:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Add your doToElementsInArray() function here:
-
+const doToElementsInArray = (arr, callback) => arr.forEach(callback);
 
 // Add your changeCompletely() function here:
+
+const changeCompletely = (e, i, arr) => arr[i] = Math.floor(Math.random() * 100 + 2).toString() + ` ${arr[i]}s!!!`
--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,17 @@
+// Add your doToElementsInArray() function here:
+const doToElementsInArray = (arr, callback) => arr.forEach(callback);
+
+// Add your changeCompletely() function here:
+
+const changeCompletely = (e, i, arr) => arr[i] = Math.floor(Math.random() * 100 + 2).toString() + ` ${arr[i]}s!!!`
+
+
+
+
+
+
+
+
 const fs = require('fs');
 const path = require('path');
 
